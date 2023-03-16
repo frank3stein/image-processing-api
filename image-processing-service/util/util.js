99c3343c36bb5298c23c633645c58abd4c29b338
Emil <martinov.emil@gmail.com>
2023-03-16 22:10:52 +0100

    Images updated, image request first turned to buffer to process it with Jimp to circumvent some errors

--- image-processing-service/util/util.js
+++ image-processing-service/util/util.js
@@ -1,22 +1,22 @@
 import fs from "fs";
 import Jimp from "jimp";
 import path from "path";
+import fetch from "node-fetch";
 
-
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
- export async function filterImageFromURL(inputURL) {
+export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const res = await fetch(inputURL);
+      // I turn the image to a buffer and then to a Jimp image, otherwise some images are not processed
+      const data = await res.arrayBuffer();
+      const photo = await Jimp.read(data);
+      // Used the new nodejs URL to get the path of the file as I am using modules
       const relativePath = new URL(path.dirname(import.meta.url)).pathname;
-      const outpath = relativePath +
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      const outpath =
+        relativePath +
+        "/tmp/filtered." +
+        Math.floor(Math.random() * 2000) +
+        ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
@@ -35,7 +35,7 @@ import path from "path";
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
- export async function deleteLocalFiles(files) {
+export async function deleteLocalFiles(files) {
   for (let file of files) {
     fs.unlinkSync(file);
   }
